b1e12307ef55cf6526c74b2ff03962edfb0eeda5
Néo Abric <abricneo@gmail.com>
2022-09-28 14:50:12 +0200

    ajout de 1 div

--- script.js
+++ script.js
@@ -2,4 +2,11 @@ const myElement = document.getElementById('conteneur');
 
 console.log(myElement);
 
+/*AJOUT DE DIV*/
+const ajoutDiv = document.createElement('div');
 
+let recupDiv = document.getElementById('conteneur');
+
+recupDiv.appendChild(ajoutDiv);
+
+ajoutDiv.innerHTML = ("<p>Montpellier</p>")
