ce868b0d8fb71fd78706f0df08cd0e8664cef6a4
Néo Abric <abricneo@gmail.com>
2022-09-28 15:43:16 +0200

    evenetListener comptage du nombre de clique sur bouton

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ console.log(myElement);
 /*AJOUT DE DIV*/
 
 
-for (i = 0; i < 2; i++){
+for (i = 0; i < 5; i++){
     const ajoutDiv = document.createElement('div');
     ajoutDiv.setAttribute('id', 'element')
 
@@ -15,3 +15,20 @@ for (i = 0; i < 2; i++){
     
     ajoutDiv.innerHTML = ("<p>Montpellier</p>");
 }
+
+
+const boutonClick = document.getElementById('boutonAdd')
+let comptageClick = 0
+ boutonClick.addEventListener('click', function(){
+    comptageClick = comptageClick+1;
+    console.log("tu as cliqué " + comptageClick)
+    
+    //console.log("click")
+    /*for(let click = 0; click <  1; click++ ){
+        console.log("tu as cliqué" + [click+1])
+    }*/
+ });
+
+
+
+
