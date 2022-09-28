82d5b0df1442f893bdc6ece720288bead5cddaf1
Néo Abric <abricneo@gmail.com>
2022-09-28 14:49:29 +0200

    affichage de toutes les div réussi

--- script.js
+++ script.js
@@ -1,15 +1,5 @@
-const myElement = document.getElementsByClassName('element');
+const myElement = document.getElementById('conteneur');
+
 console.log(myElement);
 
-for (let i = 0; i < myElement.lenght; i++){
-    console.log(myElement)
-}
 
-/*AJOUT DE DIV*/
-const ajoutDiv = document.createElement('div');
-
-let recupDiv = document.getElementById('conteneur');
-
-recupDiv.appendChild(ajoutDiv);
-
-ajoutDiv.innerHTML = ("<p>Montpellier</p>")
